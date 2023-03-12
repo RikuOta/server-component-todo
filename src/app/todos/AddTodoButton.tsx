c50df154a304886cf4e965b302b5c333295d7f1f
RikuOta <otarikuweb@gmail.com>
2023-03-12 20:20:05 +0900

    fix: Fixed to be updated after addition

--- src/app/todos/AddTodoButton.tsx
+++ src/app/todos/AddTodoButton.tsx
@@ -1,19 +1,26 @@
 'use client'
 
-import React from 'react'
+import { useRouter } from 'next/navigation'
+import React, { useTransition } from 'react'
 import axios from 'axios'
 
 export const AddTodoButton = () => {
+  const router = useRouter()
+  const [isPending, startTransition] = useTransition()
+
   const handleAdd = async () => {
     const input = prompt()
     axios.post('http://localhost:3000/api/todos', {
       name: input,
     })
+    startTransition(() => {
+      router.refresh();
+    });
   }
 
   return (
-    <button onClick={() => handleAdd()} className="text-white bg-blue-500 px-6 py-2 rounded-md mb-4">
-      Add Todo
+    <button onClick={handleAdd} className="text-white bg-blue-500 px-6 py-2 rounded-md mb-4">
+      {isPending ? 'During addition...' : 'Add Todo'}
     </button>
   )
 }
